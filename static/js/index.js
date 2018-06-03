99bf32792d8652e17901a4fe24dc6695608a6e93
Shota Sawada <shota.sawada@crowdworks.co.jp>
2018-06-04 00:12:06 +0900

    add: fallback js for iOS

--- static/js/index.js
+++ static/js/index.js
@@ -17,3 +17,28 @@ const externalLinkAddBlank = function () {
 }
 // var a__add_blanks = external_link__add_blank(); console.log(a__add_blanks);
 document.addEventListener('DOMContentLoaded', externalLinkAddBlank, false)
+
+/**
+ * Show Webp as jpg in iOS
+ * @param path
+ * @returns {boolean}
+ */
+const isWebp = path => path.endsWith('.webp')
+
+const images = document.querySelectorAll('img')
+
+const wrapWebpPicture =  () => {
+  images.forEach(image => {
+    const src = image.src
+    if (isWebp(src)) {
+      const jpgSrc = src.replace(/\.webp/, '.jpg')
+
+      const newImage = document.createElement('picture')
+      newImage.innerHTML = `<source type="image/webp" srcset="${src}"><img src="${jpgSrc}">`
+      image.parentNode.insertBefore(newImage, image)
+      image.remove()
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', wrapWebpPicture, false)
