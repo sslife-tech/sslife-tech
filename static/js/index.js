e7df87e3b1c6858875e59b76cd73fa294cfbb0ac
Shota Sawada <shota.sawada@crowdworks.co.jp>
2018-06-09 13:23:34 +0900

    両家顔合わせコンテンツ追加

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,23 @@
 /**
+ * Add _blank to outbound
+ * @returns {*}
+ */
+const externalLinkAddBlank = function () {
+  const a_tags = document.querySelectorAll('a:not([target="_blank"])')
+  let res = []
+  if (!a_tags.length) return // a_tagsがマッチなし = end
+  for (let i = 0; i < a_tags.length; i++) {
+    if (a_tags[i].href.indexOf(window.location.host) !== -1) continue
+    if (a_tags[i].href.indexOf('#') === 1) continue
+    a_tags[i].setAttribute('target', '_blank')
+    a_tags[i].setAttribute('rel', 'noopener')
+    res.push(a_tags[i])
+  }
+  return res
+}
+document.addEventListener('DOMContentLoaded', externalLinkAddBlank, false)
+
+/**
  * Show Webp as jpg in iOS
  * @param path
  * @returns {boolean}
